a77fc7c8911aaf7d079f9ed21da3044eac197578
nat.sds <nathaliasoaressouza@gmail.com>
2025-05-08 00:10:43 -0300

    Atividade finalizada.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@
 const readline = require('readline-sync');
 
 // Capturando o número de pessoas na mesa
-const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
+const numeroPessoas = readline.questionInt('Insira o numero de pessoas na mesa: ');
 
 // Capturando o valor total da conta
-const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
+const valorTotal = readline.questionFloat('Insira o valor total da conta: ');
 
 // Capturando o método de pagamento
-const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
-
+const metodoPagamento = readline.questionInt('Insira o metodo de pagamento (1- PIX, 2- dinheiro, 3- cartao): ');
 
 //Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+let valorFinal = valorTotal;
+if (metodoPagamento === 1 || metodoPagamento === 2) {
+    valorFinal = valorTotal * 0.9;
+}
 
+else {
+    valorFinal = valorTotal;
+}
+
+const valorPorPessoa = valorFinal / numeroPessoas;
 
 // Exibindo os resultados
+
+console.log("Método de pagamento escolhido: " + metodoPagamento);
+console.log("Valor total com desconto (se aplicável): R$ " + valorFinal.toFixed(2));
+console.log("Cada pessoa deverá pagar: R$ " + valorPorPessoa.toFixed(2));
